ae8322209e9ead551447e91c4f0abcb698f7d827
Sylwia <sorzeszkowska@gmail.com>
2018-09-23 21:54:20 +0200

    zadanie6gwiazdki

---
+++ src/Zadanie6choinka.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Zadanie6choinka {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		int liczbaGwiazdek = 4;
+
+		System.out.println("Podaj liczbe gwiazdek");
+		liczbaGwiazdek = Integer.parseInt(bufferedReader.readLine());
+
+		for (int i = 0; i < liczbaGwiazdek; i++) {
+
+			for (int j = 0; j < liczbaGwiazdek; j++) {
+				if (i == 0 || j == 0 || i == liczbaGwiazdek - 1 || j == liczbaGwiazdek - 1) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
