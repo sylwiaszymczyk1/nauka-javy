6532a83e3c99718a7c24e7cb9b21d5f54bde5329
Sylwia <sorzeszkowska@gmail.com>
2018-09-27 22:04:41 +0200

    zadanie 9 for i tablice

--- src/Zadanie9Array.java
+++ src/Zadanie9Array.java
@@ -4,12 +4,33 @@ public class Zadanie9Array {
 	public static void main(String[] args) {
 
 		int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		// wyswietlenie pierwszej tabeli
 		for (int i = 0; i < myArray.length; i++) {
 			System.out.println(myArray[i] + " ");
 		}
+		// zrobienie drugiej tabeli
 		int[] myArray2 = new int[10];
-		for (int i=9; i<=0; i--) {
-			
+		for (int i = 9; i >= 0; i--) {
+			myArray2[i] = 9 - i;
+
+		}
+		int[] myArray3 = new int[10];
+		for (int i = 0; i < myArray3.length; i++) {
+			myArray3[i] = myArray[i] + myArray2[i];
+
+		}
+
+		// wyswietlenia drugiej tabeli
+		printArray(myArray2);
+
+		// wyswietlenie trzeciej tabeli
+		printArray(myArray3);
+	}
+
+	public static void printArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("Element " + (i+1) +": " + array[i]);
+
 		}
 	}
 
