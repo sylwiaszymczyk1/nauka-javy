23d8f16c63f0d420aa99b30ceb1ed76b04e4b11f
Sylwia <sorzeszkowska@gmail.com>
2018-10-06 21:33:34 +0200

    obliczenie sumy i sredniej elementow tablicy

--- src/Zadanie10array.java
+++ src/Zadanie10array.java
@@ -17,11 +17,33 @@ public class Zadanie10array {
 		myArray = new int[arraySize];
 
 		Random randomGenerator = new Random();
-		for (int i=0; i<arraySize; i++) {
-			myArray[i]= randomGenerator.nextInt(10000);//znajdz liczbe min i max , srednia
-			System.out.println(myArray[i]);
+		int min = 0;
+		int max = 0;
+		int sum = 0;
+		int average = 0;
+		for (int i = 0; i < arraySize; i++) {
+			myArray[i] = randomGenerator.nextInt(10000);// znajdz liczbe min i max , srednia
+			if (i == 0) {
+				min = myArray[i];
+				max = myArray[i];
+			}
+			if (myArray[i] < min) {
+				min = myArray[i];
+			}
+			if (myArray[i] > max) {
+				max = myArray[i];
+			}
+
+			sum += myArray[i];
+			
+			average=sum/arraySize;
+			
+			System.out.println(myArray[i] + " Wartosc minimalna to " + min + " Wartosc maksymalna to " + max);
+			
 		}
+		System.out.println("Suma elementow tablicy wynosi " + sum);
+		System.out.println("Srednia elementów tablicy wynosi " + average);
+
 	}
-		
 
 }
